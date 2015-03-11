7ac1090ecc5e5e1295e209ee4998b7c8cc210f00
Jar <jar@jardeMacBook-Pro.local>
2015-03-11 23:03:27 +0800

    Add sequelize ORM framework and mysql module

--- xuemo/routes/users.js
+++ xuemo/routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+  var user = {
+  	id:1,
+  	name: 'Jar'
+  };
+  res.json(user);
 });
+
+router.get('/:userId', function(req, res) {
+	res.send(req.params.userId);
+})
 
 module.exports = router;
