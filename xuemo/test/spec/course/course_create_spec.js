f040d53deb63d4cdaaacccedbab75ffb5b036f2d
Jar-18 <jar.an.cn@gmail.com>
2015-05-03 01:21:35 +0800

    Course create api test case

---
+++ xuemo/test/spec/course/course_create_spec.js
@@ -0,0 +1,71 @@
+var frisby = require('frisby');
+var config = require('../../config/config.json');
+
+frisby.globalSetup({ // globalSetup is for ALL requests
+	request: {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}
+});
+
+console.log(config);
+frisby.create('Create course in step 1')
+	.post(config.HOST_URL + '/courses', {
+		title: "Course created by frisby",
+		describe: "Desceibe...",
+		price: 100,
+		teacher: {
+			id: 1
+		},
+		category: {
+			id: 1
+		},
+		districts: [{
+			id: 1
+		}, {
+			id: 2
+		}],
+		sites: [{
+			id: 1
+		}, {
+			id: 2
+		}],
+		types: [{
+			id: 1
+		}, {
+			id: 2
+		}]
+	}, {
+		json: true
+	})
+	.expectStatus(201)
+	.expectJSONTypes({
+		status: String,
+		courseId: Number
+	})
+	.expectJSON({
+		status: 'Success'
+	})
+	.afterJSON(function(result) {
+		frisby.create("Create course in step 2")
+			.put(config.HOST_URL + '/courses/' + result.courseId, {
+				describe: "Desceibe...",
+				pics: [{
+					id: "default.jpg"
+				}, {
+					id: "default2.jpg"
+				}]
+			}, {
+				json: true
+			})
+			.expectStatus(201)
+			.expectJSONTypes({
+				status: String
+			})
+			.expectJSON({
+				status: 'Success'
+			})
+			.toss();
+	})
+	.toss()
